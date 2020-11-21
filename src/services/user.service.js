7f57672942a433d1bf9af3abd6bce6830709fab9
Rebecca <asryan.rebecca@gmail.com>
2020-11-21 19:55:55 +0400

    feat(SignIn): Send request to server to get access token

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -4,19 +4,5 @@ import client from './client';
   User Authentication
 */
 export const signInApi = ({ params }) => {
-  // TODO: remove this when the api is ready
-  // simulate successful sign in
-  return new Promise((resolve, reject) => {
-    setTimeout(
-      () =>
-        resolve({
-          data: {
-            success: true,
-            data: 'token',
-          },
-        }),
-      1000,
-    );
-  });
   client().post('/sign-in', params);
 };
