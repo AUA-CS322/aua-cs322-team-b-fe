9ad2386eb5bb8dfbf6f458d58f0c0fd6b74013a6
nanedadalyan <nane.dadalyan@gmail.com>
2020-12-07 11:43:32 +0400

    feat(organization-chart)

--- src/containers/Profile.container.js
+++ src/containers/Profile.container.js
@@ -20,7 +20,6 @@ import Chart from '../components/Organization/Chart';
 import UserSearch from '../components/Organization/UserSearch';
 import Logo from '../components/shared/Logo';
 import debounce from 'debounce';
-
 const Profile = () => {
   const [selectedUserId, setSelectedUserId] = useState();
   const dispatch = useDispatch();
@@ -28,29 +27,24 @@ const Profile = () => {
   const selectedUser = useSelector(getUser);
   const chartData = useSelector(getUserChart);
   const chartLoading = useSelector(getUserChartLoading);
-
   useEffect(() => {
     dispatch(getUserAction.request());
     dispatch(getUserChartAction.request());
   }, []);
-
   useEffect(() => {
     if (selectedUser?.id) {
       setSelectedUserId(selectedUser.id);
     }
   }, [selectedUser]);
-
   const handleSearch = debounce(value => {
     if (!value || value.length < 3) {
       return;
     }
     dispatch(searchUserAction.request({ query: value }));
   }, 200);
-
   const handleSelect = id => {
     dispatch(getUserAction.request({ id }));
   };
-
   return (
     <Container top={30}>
       <Row justify="space-between" align="middle">
@@ -61,7 +55,6 @@ const Profile = () => {
           <UserSearch onSearch={handleSearch} onSelect={handleSelect} />
         </Col>
       </Row>
-
       {loading || chartLoading ? (
         <MainLoader />
       ) : (
@@ -70,12 +63,15 @@ const Profile = () => {
             <UserDetails {...selectedUser} />
           </Col>
           <Col span={10}>
-            <Chart data={chartData} userId={selectedUserId} />
+            <Chart
+              data={chartData}
+              userId={selectedUserId}
+              onSelect={handleSelect}
+            />
           </Col>
         </Row>
       )}
     </Container>
   );
 };
-
 export default Profile;
